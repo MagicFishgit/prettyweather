4a260979df3696d14751d7ca43f4c6aaea2f6a9e
Rudi Visagie <rudivisagie@icloud.com>
2022-10-31 13:55:21 +0200

    Extracted and formatted data I want to use

--- website/views.py
+++ website/views.py
@@ -10,13 +10,27 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=["GET", "POST"])
 def base():
     if request.method == "POST":
-        searchLocation = request.form.get('searchTerm')
-        request_url = f"{BASE_URL}?appid={API_KEY}&q={searchLocation}"
+        search_location = request.form.get('searchTerm')
+        request_url = f"{BASE_URL}?appid={API_KEY}&q={search_location}"
         response = requests.get(request_url)
 
         if response.status_code == 200:
             data = response.json()
+            weather = data['weather'][0]['description']
+            temperature = round(data['main']['temp'] - 273.15)
+            feels_like = round(data['main']['feels_like'] - 273.15)
+            temp_min = round(data['main']['temp_min'] - 273.15)
+            temp_max = round(data['main']['temp_min'] - 273.15)
+            pressure = data['main']['pressure']
+            humidity = data['main']['humidity']
+            visibility = data['visibility']
+            wind_speed = data['wind']['speed']
+            cloud_percentage = data['clouds']['all']
+            country = data['sys']['country']
+            city = data['name']
             print(data)
+            print(weather)
+            print(temperature)
         else:
             print("Error");
 
