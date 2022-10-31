ecb02c6ab261b6d842bb58acd3dcdd0bcbe79891
Rudi Visagie <rudivisagie@icloud.com>
2022-10-31 18:32:56 +0200

    Fixed bug: Did not have default long lat specified if not POST request. Removed unused JS from app.js

--- website/static/app.js
+++
@@ -1,8 +0,0 @@
-function loadMap(){
-    let params = {
-        center: new google.maps.LatLng(53, -6), zoom :4,
-        disableDefaultUI: true
-    };
-
-    let map = new google.maps.Map(document.getElementById("map-background"), params)
-}
